31935ebc323f09f25adc15e9d967de4723135803
harbaouiiii <harbeouimohamed@gmail.com>
2023-01-07 17:25:25 +0100

    fixed database connection

--- src/main/java/com/formalab/ecommerce/EcommerceApplication.java
+++ src/main/java/com/formalab/ecommerce/EcommerceApplication.java
@@ -1,15 +1,27 @@
 package com.formalab.ecommerce;
 
+import com.formalab.ecommerce.dao.RoleRepository;
+import com.formalab.ecommerce.model.Role;
+import com.formalab.ecommerce.model.RoleName;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 public class EcommerceApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(EcommerceApplication.class, args);
+
+		/*RoleRepository roleRepository = applicationContext.getBean(RoleRepository.class);
+
+		roleRepository.save(new Role(RoleName.ROLE_ADMIN));
+		roleRepository.save(new Role(RoleName.ROLE_PM));
+		roleRepository.save(new Role(RoleName.ROLE_USER));*/
+
 	}
 
 }
